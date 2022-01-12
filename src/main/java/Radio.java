2882ee17811c6b828c5f772c3682b9dcd3c16491
Stjushenka <stjushenka@gmail.com>
2022-01-12 15:53:47 +0300

    flexible

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -2,11 +2,22 @@ public class Radio {
 
     int minRadio;
     int maxRadio;
-    private int currentVolume;
     int minVolume;
     int maxVolume;
+    public int currentVolume;
     public int currentStation;
-    public int radioStation;
+
+    public Radio() {
+    }
+
+    public Radio(int minRadio, int maxRadio, int minVolume, int maxVolume, int currentStation, int currentVolume) {
+        this.minRadio = minRadio;
+        this.maxRadio = maxRadio;
+        this.minVolume = minVolume;
+        this.maxVolume = maxVolume;
+        this.currentStation = currentStation;
+        this.currentVolume = currentVolume;
+    }
 
     public void setCurrentStation(int currentStation) {
         this.currentStation = currentStation;

f2d99e68a0b826bbf98fcc0e8ad3299296af14e9
Stjushenka <stjushenka@gmail.com>
2022-01-12 15:55:11 +0300

    lom

--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -1,61 +1,58 @@
 import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class RadioTest {
 
-
     @Test
     public void shouldUseConstructor() {
-        Radio rad5 = new Radio(0, 9, 0, 100, 50, 5);
+        Radio rad1 = new Radio(0,9, 0,100,5, 50);
 
-        assertEquals(5, rad5.currentStation);
+        assertEquals(5, rad1.currentStation);
 
-        Radio rad1 = new Radio(0, 9, 0, 100, 100, 9);
+        Radio rad2 = new Radio(0,9, 0,100,9, 50);
 
-        assertEquals(100, rad1.currentVolume);
+        assertEquals(9, rad2.currentStation);
 
-        Radio rad2 = new Radio(0, 9, 0, 100, 0, 0);
+        Radio rad3 = new Radio(0,9, 0,100,5, 100);
 
-        assertEquals(0, rad2.currentStation);
+        assertEquals(100, rad3.currentVolume);
 
-        Radio rad3 = new Radio(0, 9, 0, 100, 0, 0);
+        Radio rad4 = new Radio(0,9, 0,100,5, 0);
 
-        assertEquals(0, rad3.currentVolume);
-
-        Radio rad4 = new Radio(0, 9, 0, 100, 0, 9);
-
-        assertEquals(9, rad4.currentStation);
+        assertEquals(0, rad4.currentVolume);
     }
 
 
     @Test
     public void setCurrentStation() {
-        Radio rad = new Radio(0,9, 0, 100, 9, 9);
-        int actual = rad.currentStation;
+        Radio rad = new Radio(0,9, 0,100,9, 50);
+        int currentStation = 9;
+        int actual = currentStation;
         assertEquals(9, actual);
     }
 
     @Test
     public void setCurrentStation2() {
-        Radio rad = new Radio(0,9,0,100,50,0);
-        int actual = rad.currentStation;
+        Radio rad = new Radio(0,9, 0,100,0, 50);
+        int currentStation = 0;
+        int actual = currentStation;
         assertEquals(0, actual);
     }
 
     @Test
     public void nextStation() {
-        Radio rad = new Radio(0,9,0,100,50,9);
+        Radio rad = new Radio(0,9, 0,100,9, 50);
         int actual = rad.nextStation(9);
         assertEquals(0, actual);
     }
 
     @Test
     public void nextStation2() {
-        Radio rad = new Radio(0,9,0,100,50,0);
-        rad.minRadio = 0;
-        rad.maxRadio = 9;
+        Radio rad = new Radio(0,9, 0,100,0, 50);
         int actual = rad.nextStation(0);
         assertEquals(1, actual);
     }
@@ -63,44 +60,44 @@ class RadioTest {
 
     @Test
     public void prewStation() {
-        Radio rad = new Radio(0,9,0,100,50,0);
+        Radio rad = new Radio(0,9, 0,100,0, 50);
         int actual = rad.prewStation(0);
         assertEquals(9, actual);
     }
 
     @Test
     public void prewStation2() {
-        Radio rad = new Radio(0,9,0,100,50,9);
+        Radio rad = new Radio(0,9, 0,100,9, 50);
         int actual = rad.prewStation(9);
         assertEquals(8, actual);
     }
 
     @Test
     public void volumeUp() {
-        Radio rad = new Radio(0,9,0,100,100,0);
-        int actual = rad.volumeUp(100);
-        assertEquals(100, actual);
+        Radio rad = new Radio(0,9, 0,100,5, 10);
+        int actual = rad.volumeUp(10);
+        assertEquals(11, actual);
     }
 
     @Test
     public void volumeUp2() {
-        Radio rad = new Radio(0,9,0,100,0,0);
+        Radio rad = new Radio(0,9, 0,100,5, 0);
         int actual = rad.volumeUp(0);
         assertEquals(1, actual);
     }
 
     @Test
     public void volumeDown() {
-        Radio rad = new Radio(0,9,0,100,0,0);
+        Radio rad = new Radio(0,9, 0,100,5, 0);
         int actual = rad.volumeDown(0);
         assertEquals(0, actual);
     }
 
     @Test
     public void volumeDown2() {
-        Radio rad = new Radio(0,9,0,100,100,0);
-        int actual = rad.volumeDown(100);
-        assertEquals(99, actual);
+        Radio rad = new Radio(0,9, 0,100,5, 10);
+        int actual = rad.volumeDown(10);
+        assertEquals(9, actual);
     }
 
 }
